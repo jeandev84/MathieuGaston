40c31248283410c930e8a9050a318d01059d6f59
Yao Jean-Claude <jeandev84@yahoo.com>
2020-03-12 05:18:40 +0300

    Role Users

--- GestionVoitures/src/Controller/HomeController.php
+++ GestionVoitures/src/Controller/HomeController.php
@@ -2,8 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\Utilisateur;
+use App\Form\InscriptionType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 /**
  * Class HomeController
@@ -18,4 +25,53 @@ class HomeController extends AbstractController
     {
         return $this->render('home/index.html.twig');
     }
+
+
+    /**
+     * @Route("/inscription", name="inscription")
+     * @param Request $request
+     * @param EntityManagerInterface $em
+     * @param UserPasswordEncoderInterface $encoder
+     * @return Response
+     */
+    public function register(Request $request, EntityManagerInterface $em, UserPasswordEncoderInterface $encoder)
+    {
+        $utilisateur = new Utilisateur();
+        $form = $this->createForm(InscriptionType::class, $utilisateur);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+             $passwordCrypte = $encoder->encodePassword($utilisateur, $utilisateur->getPassword());
+             $utilisateur->setPassword($passwordCrypte);
+             $utilisateur->setRoles('ROLE_USER');
+             $em->persist($utilisateur);
+             $em->flush();
+             return $this->redirectToRoute('accueil');
+        }
+
+        return $this->render('home/register.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+
+    /**
+     * @Route("/login", name="login")
+     * @param AuthenticationUtils $utils
+     * @return Response
+     */
+    public function login(AuthenticationUtils $utils)
+    {
+        return $this->render('home/login.html.twig', [
+            'lastUsername' => $utils->getLastUsername(),
+            'error' => $utils->getLastAuthenticationError()
+        ]);
+    }
+
+
+    /**
+     * @Route("/logout", name="logout")
+    */
+    public function logout() {}
 }
---
+++ GestionVoitures/src/Entity/Utilisateur.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UtilisateurRepository")
+ * @UniqueEntity(
+ *  fields = {"username"},
+ *  message="L'utilisateur exist deja!"
+ * )
+*/
+class Utilisateur implements UserInterface
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @var string
+    */
+    private $password;
+
+    /**
+     * @Assert\EqualTo(
+     *     propertyPath="password",
+     *     message="Les mots de passe ne correspondent pas!"
+     * )
+    */
+    private $confirmPassword;
+
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $roles;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getConfirmPassword()
+    {
+        return $this->confirmPassword;
+    }
+
+    /**
+     * @param mixed $confirmPassword
+     * @return Utilisateur
+     */
+    public function setConfirmPassword($confirmPassword)
+    {
+        $this->confirmPassword = $confirmPassword;
+        return $this;
+    }
+
+
+    public function getRoles(): array
+    {
+        return [$this->roles];
+    }
+
+    public function setRoles(string $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+    */
+    public function getSalt()
+    {
+        // TODO: Implement getSalt() method.
+    }
+
+    /**
+     * @inheritDoc
+    */
+    public function eraseCredentials()
+    {
+        // TODO: Implement eraseCredentials() method.
+    }
+}
